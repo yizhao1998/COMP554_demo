ee7d224298ef7c817e37056212b5eabda632697c
Yi Zhao <11612917@mail.sustech.edu.cn>
2021-12-11 22:24:44 -0600

    update site

--- demo.js
+++ demo.js
@@ -1,4 +1,19 @@
-function make_map(cityname, dom_id) {
+async function make_map(cityname, dom_id) {
+	let map = {'AN': 'Andaman and Nicobar Islands', 'AP': 'Andhra Pradesh', 'AR': 'Arunachal Pradesh', 'AS': 'Assam', 'BR': 'Bihar', 'CH': 'Chandigarh', 'CT': 'Chhattisgarh', 'DN': 'Dadra and Nagar Haveli', 'DD': 'Daman and Diu', 'DL': 'Delhi', 'GA': 'Goa', 'GJ': 'Gujarat', 'HR': 'Haryana', 'HP': 'Himachal Pradesh', 'JK': 'Jammu and Kashmir', 'JH': 'Jharkhand', 'KA': 'Karnataka', 'KL': 'Kerala', 'LD': 'Lakshadweep', 'MP': 'Madhya Pradesh', 'MH': 'Maharashtra', 'MN': 'Manipur', 'ML': 'Meghalaya', 'MZ': 'Mizoram', 'NL': 'Nagaland', 'OR': 'Odisha', 'PY': 'Puducherry', 'PB': 'Punjab', 'RJ': 'Rajasthan', 'SK': 'Sikkim', 'TN': 'Tamil Nadu', 'TG': 'Telangana', 'TR': 'Tripura', 'UP': 'Uttar Pradesh', 'UT': 'Uttarakhand', 'WB': 'West Bengal'};
+	let resp = await $.get("https://data.covid19india.org/v4/min/data.min.json");
+	let data = [];
+	let mi = 1E9, ma = 0;
+	for (let state in map) {
+		if (state in resp) {
+			mi = Math.min(mi, resp[state]['total']['confirmed']);
+			ma = Math.max(ma, resp[state]['total']['confirmed']);
+			data.push({
+				name: map[state],
+				value: resp[state]['total']['confirmed']
+			});
+		}
+	}
+	console.log(data);
 	achart = echarts.init(document.getElementById(dom_id));
 	var option = {
 		tooltip: {
@@ -16,8 +31,8 @@ function make_map(cityname, dom_id) {
 		},
 		visualMap: {
 			left: 'right',
-			min: 500000,
-			max: 38000000,
+			min: mi,
+			max: ma,
 			inRange: {
 				color: [
 					'#313695',
@@ -40,7 +55,8 @@ function make_map(cityname, dom_id) {
 		"series": [
 			{
 				"mapType": cityname,
-				"data": [{ 'name': 'Andaman and Nicobar Islands', 'value': 4483413 }, { 'name': 'Andhra Pradesh', 'value': 4569654 }, { 'name': 'Arunachal Pradesh', 'value': 14019562 }, { 'name': 'Assam', 'value': 31171356 }, { 'name': 'Bihar', 'value': 37576326 }, { 'name': 'Chandigarh', 'value': 37545621 }, { 'name': 'Chhattisgarh', 'value': 22215351 }, { 'name': 'Dadra and Nagar Haveli', 'value': 23470312 }, { 'name': 'Delhi', 'value': 2126665 }, { 'name': 'Goa', 'value': 6744515 }, { 'name': 'Gujarat', 'value': 4967512 }, { 'name': 'Haryana', 'value': 1237446 }, { 'name': 'Himachal Pradesh', 'value': 36795059 }, { 'name': 'Jammu and Kashmir', 'value': 14056742 }, { 'name': 'Jharkhand', 'value': 12615912 }, { 'name': 'Karnataka', 'value': 17988484 }, { 'name': 'Kerala', 'value': 35552214 }, { 'name': 'Madhya Pradesh', 'value': 34288998 }, { 'name': 'Maharashtra', 'value': 21595291 }, { 'name': 'Manipur', 'value': 31374173 }, { 'name': 'Meghalaya', 'value': 25697045 }, { 'name': 'Mizoram', 'value': 16918272 }, { 'name': 'Nagaland', 'value': 11328468 }, { 'name': 'Odisha', 'value': 25935213 }, { 'name': 'Pondicherry', 'value': 787277 }, { 'name': 'Punjab', 'value': 27916410 }, { 'name': 'Rajasthan', 'value': 1248022 }, { 'name': 'Sikkim', 'value': 32758611 }, { 'name': 'Tamil Nadu', 'value': 24989925 }, { 'name': 'Telangana', 'value': 35555339 }, { 'name': 'Tripura', 'value': 25281906 }, { 'name': 'Uttarakhand', 'value': 15479123 }, { 'name': 'Uttar Pradesh', 'value': 20774596 }, { 'name': 'West Bengal', 'value': 30262964 }],
+				// "data": [{ 'name': 'Andaman and Nicobar Islands', 'value': 4483413 }, { 'name': 'Andhra Pradesh', 'value': 4569654 }, { 'name': 'Arunachal Pradesh', 'value': 14019562 }, { 'name': 'Assam', 'value': 31171356 }, { 'name': 'Bihar', 'value': 37576326 }, { 'name': 'Chandigarh', 'value': 37545621 }, { 'name': 'Chhattisgarh', 'value': 22215351 }, { 'name': 'Dadra and Nagar Haveli', 'value': 23470312 }, { 'name': 'Delhi', 'value': 2126665 }, { 'name': 'Goa', 'value': 6744515 }, { 'name': 'Gujarat', 'value': 4967512 }, { 'name': 'Haryana', 'value': 1237446 }, { 'name': 'Himachal Pradesh', 'value': 36795059 }, { 'name': 'Jammu and Kashmir', 'value': 14056742 }, { 'name': 'Jharkhand', 'value': 12615912 }, { 'name': 'Karnataka', 'value': 17988484 }, { 'name': 'Kerala', 'value': 35552214 }, { 'name': 'Madhya Pradesh', 'value': 34288998 }, { 'name': 'Maharashtra', 'value': 21595291 }, { 'name': 'Manipur', 'value': 31374173 }, { 'name': 'Meghalaya', 'value': 25697045 }, { 'name': 'Mizoram', 'value': 16918272 }, { 'name': 'Nagaland', 'value': 11328468 }, { 'name': 'Odisha', 'value': 25935213 }, { 'name': 'Pondicherry', 'value': 787277 }, { 'name': 'Punjab', 'value': 27916410 }, { 'name': 'Rajasthan', 'value': 1248022 }, { 'name': 'Sikkim', 'value': 32758611 }, { 'name': 'Tamil Nadu', 'value': 24989925 }, { 'name': 'Telangana', 'value': 35555339 }, { 'name': 'Tripura', 'value': 25281906 }, { 'name': 'Uttarakhand', 'value': 15479123 }, { 'name': 'Uttar Pradesh', 'value': 20774596 }, { 'name': 'West Bengal', 'value': 30262964 }],
+				"data": data,
 				"name": "",
 				"symbol": "circle",
 				"type": "map",
